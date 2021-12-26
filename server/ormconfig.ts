4ae51f8e79e83eba50488b0f834bf236b0bbd437
Miftah Salam <salam.miftah@gmail.com>
2021-12-26 19:12:10 +0700

    edit ormconfig add ssl options

--- server/ormconfig.ts
+++ server/ormconfig.ts
@@ -33,7 +33,9 @@ module.exports = [
     synchronize: true,
     ssl: true,
     extra: {
-      rejectUnauthorized: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
   },
 ]
